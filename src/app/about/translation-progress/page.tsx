7cce6e0d04e70b35c669298efe378be8f99ede06
Matt Gross <mattgrosswork@gmail.com>
2025-05-16 15:23:57 -0700

    fix: use vercel-specific env url for progress data

--- src/app/about/translation-progress/page.tsx
+++ src/app/about/translation-progress/page.tsx
@@ -3,7 +3,7 @@ import Content from './content.mdx';
 import './page.css';
 
 export default async function TranslationProgressPage({}) {
-  const baseUrl = process.env.URL ?? '';
+  const baseUrl = (process.env.URL || process.env.VERCEL_URL) ?? '';
   const progressData = await fetch(baseUrl + '/api/meta/translation-progress');
   const progress = await progressData.json();
 
