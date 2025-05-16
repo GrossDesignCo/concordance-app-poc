4a39a65cb145c84720d127cc6daf2a6599cbb713
Matt Gross <mattgrosswork@gmail.com>
2025-05-16 16:04:46 -0700

    fix: turn off caching for endpoint

--- src/app/api/meta/translation-progress/route.ts
+++ src/app/api/meta/translation-progress/route.ts
@@ -1,18 +1,17 @@
+import progress from '@/data/meta/translation-progress.json';
 import { NextResponse } from 'next/server';
-import fs from 'fs';
-import path from 'path';
 
-const PROGRESS_PATH = path.join(process.cwd(), 'src/data/meta/translation-progress.json');
-
+/**
+ * Statically serve data for the translation progress meter/viz
+ * @returns a `TranslationProgress` object
+ */
 export async function GET() {
   try {
-    const file = fs.readFileSync(PROGRESS_PATH, 'utf-8');
-    const progress = JSON.parse(file);
     return NextResponse.json(progress, {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
-        'Cache-Control': 'public, s-maxage=3600, stale-while-revalidate=86400',
+        // 'Cache-Control': 'public, s-maxage=3600, stale-while-revalidate=86400',
       },
     });
   } catch (error) {
